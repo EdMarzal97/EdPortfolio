d43b166c68b4c0c46feafd41d40b3e36f7415662
Edward Marzal <edsanchezmarzal@gmail.com>
2023-06-28 00:46:23 -0500

    styles plus my logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles.js'
+import { navLinks } from  '../constans'
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div >
+
+      </div>
+    </nav>
   )
 }
 
